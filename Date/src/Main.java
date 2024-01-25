6566ffbcb8be497a43dba9d1f67b9d090e852bf0
Adams <S02820899@ccc.ccofc.edu>
2024-01-24 18:54:52 -0700

    First Practice

---
+++ Date/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String day = "Wednesday";
+        int date = 24;
+        String month = "January";
+        int year = 2024;
+        System.out.print(day + ", " + month + " " + date + ", " + year);
+
+    }
+}
