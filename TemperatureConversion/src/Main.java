2745367612d337edcb8345675661aba7abeb8b57
Seth Adams <splatterscorlos@gmail.com>
2024-01-30 17:25:11 -0700

    Added comments for clarity

--- TemperatureConversion/src/Main.java
+++ TemperatureConversion/src/Main.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
+import java.util.Scanner; //Importing Scanner
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); //Making a new Scanner for input
+        //Defining variables for three different types for temperature
         double tempF;
         double tempC;
         double tempK;
         System.out.print("Enter a Temperature in Fahrenheit: ");
-        tempF = in.nextInt();
-        tempC = (tempF - 32.0) * (5.0 / 9.0);
-        tempK = tempC + 273.15;
+        tempF = in.nextInt(); //Prompting user input for temperature
+        tempC = (tempF - 32.0) * (5.0 / 9.0); //Conversion from Fahrenheit to Celsius
+        tempK = tempC + 273.15; //Conversion from Celsius to Kelvin
         System.out.println("Celsius: " + tempC);
         System.out.println("Kelvin: " + tempK);
     }
