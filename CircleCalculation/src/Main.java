fbbe5b39217b05f86c5fb0824b6ddf72d24b087a
Seth Adams <splatterscorlos@gmail.com>
2024-02-13 21:05:32 -0700

    Methods Homework

--- CircleCalculation/src/Main.java
+++ CircleCalculation/src/Main.java
@@ -6,8 +6,20 @@ public class Main {
         double radius;
         System.out.print("Enter the radius of a circle: ");
         radius = in.nextDouble();
+        Diameter(radius);
+        Area(radius);
+        Circumference(radius);
+    }
+
+    public static void Diameter (double radius) {
         System.out.println("Diameter: " + (radius * 2));
+    }
+
+    public static void Area (double radius) {
         System.out.println("Area: " + ((radius * radius) * Math.PI));
+    }
+
+    public static void Circumference (double radius) {
         System.out.println("Circumference: " + (radius * 2 * Math.PI));
     }
 }
