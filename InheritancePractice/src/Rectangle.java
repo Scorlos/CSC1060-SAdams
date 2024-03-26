4cd73fd8e1faa2be5b67f3b8a25697dcf75c1eba
Adams <S02820899@ccc.ccofc.edu>
2024-03-25 19:06:09 -0600

    Practice 2

---
+++ InheritancePractice/src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle extends Shape{
+    double side1;
+    double side2;
+
+    public Rectangle (double side1, double side2) {
+        
+    }
+    @Override
+    public double calculateArea() {
+        return 0;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 0;
+    }
+}
