0b34193a35ace8ac32b315b032e598e5c753ce6c
Scorlos Tarlac <splatterscorlos@gmail.com>
2024-03-25 19:34:31 -0600

    Practice for inheritance 3

--- InheritancePractice/src/Main.java
+++ InheritancePractice/src/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Circle circle1 = new Circle(5);
         Triangle triangle1 = new Triangle(6,4,4);
+        Rectangle rectangle1 = new Rectangle(5,8);
         System.out.println(circle1.calculateArea());
         System.out.println(circle1.calculatePerimeter());
         System.out.println(triangle1.calculateArea());
         System.out.println(triangle1.calculatePerimeter());
+        System.out.println(rectangle1.calculateArea());
+        System.out.println(rectangle1.calculatePerimeter());
     }
 }
--- InheritancePractice/src/Rectangle.java
+++ InheritancePractice/src/Rectangle.java
@@ -3,15 +3,16 @@ public class Rectangle extends Shape{
     double side2;
 
     public Rectangle (double side1, double side2) {
-        
+        this.side1 = side1;
+        this.side2 = side2;
     }
     @Override
     public double calculateArea() {
-        return 0;
+        return side1*side2;
     }
 
     @Override
     public double calculatePerimeter() {
-        return 0;
+        return (side1*2) + (side2*2);
     }
 }
