21a3ec2e38cfeb8b5d6496a5ca516e6ada6398e7
Seth Adams <splatterscorlos@gmail.com>
2024-04-17 19:49:49 -0600

    Recursion Practice edits

--- RecursionPractice/src/Main.java
+++ RecursionPractice/src/Main.java
@@ -6,8 +6,7 @@ public class Main {
         System.out.println(powerOf2GreaterThanN(127));
     }
 
-    public static String appendNTimes(String phrase, int numOfTimes)
-    {
+    public static String appendNTimes(String phrase, int numOfTimes) {
         if (numOfTimes >= 1) {
             return phrase + appendNTimes(phrase, numOfTimes - 1);
         }
@@ -19,9 +18,9 @@ public class Main {
     }
 
     public static int powerOf2(int num, int power) {
-    if (num < Math.pow(2,power)) {
-        return (int) Math.pow(2,power);
+        if (num < Math.pow(2, power)) {
+            return (int) Math.pow(2, power);
         }
-    return powerOf2(num, power + 1);
+        return powerOf2(num, power + 1);
     }
 }
